15d1024419baf017978096eff561eae2dc0b5b37
kalpanaDevOp <159613684+kalpanaDevOp@users.noreply.github.com>
2024-02-10 15:49:26 +0530

    Update HelloWorld.java
    a to the power of b change in HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,13 +3,15 @@ public class HelloWorld
 	public static void main(String args[]) 
 	{
 		System.out.println("Hello World! Welcome to DevOps!!!");
-		int a;
-		int b;
+		int a=10;
+		int b=5;
 		int d=a-b;
 		int e=a*b;
 		int f=a/b;
+		int g=a^b;
 		System.out.println("subtraction of a and b="+d);
 		System.out.println("multiplication of a and b="+e);
 		System.out.println("division of a and b="+f);
+		System.out.println("a to the power of b="+g);
 	}
 }
