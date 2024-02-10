64e3b06ae0e7d290a6def4bc180330c58919a29f
kalpanaDevOp <159613684+kalpanaDevOp@users.noreply.github.com>
2024-02-10 15:02:31 +0530

    Update HelloWorld.java
    added subtraction with hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,9 @@ public class HelloWorld
 	public static void main(String args[]) 
 	{
 		System.out.println("Hello World! Welcome to DevOps!!!");
+		int a;
+		int b;
+		int d=a-b;
+		System.out.println("subtraction of a and b="d);
 	}
 }
